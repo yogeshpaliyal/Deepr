3c9b28b04b1995cd77067955afe5bc834439753a
Yogesh Choudhary Paliyal <yogeshpaliyal@users.noreply.github.com>
2025-09-14 15:06:12 +0000

    Github Actions: App version Bumped to v1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.yogeshpaliyal.deepr"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
