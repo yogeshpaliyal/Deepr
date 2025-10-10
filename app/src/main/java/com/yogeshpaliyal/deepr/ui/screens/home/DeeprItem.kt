05272705e0b7f54d41f735c1fea90bd85445eeb0
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2025-10-10 12:34:59 +0530

    feat: update dropdown menu item colors for improved error visibility

--- app/src/main/java/com/yogeshpaliyal/deepr/ui/screens/home/DeeprItem.kt
+++ app/src/main/java/com/yogeshpaliyal/deepr/ui/screens/home/DeeprItem.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -286,10 +287,14 @@ fun DeeprItem(
                                 },
                             )
                             DropdownMenuItem(
+                                colors =
+                                    MenuDefaults.itemColors(
+                                        MaterialTheme.colorScheme.error,
+                                        MaterialTheme.colorScheme.error,
+                                    ),
                                 text = {
                                     Text(
                                         stringResource(R.string.delete),
-                                        color = MaterialTheme.colorScheme.onErrorContainer,
                                     )
                                 },
                                 onClick = {
