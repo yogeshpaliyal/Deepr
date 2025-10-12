47f732145adc26496638fac96ecb7bbc97a8817c
anas shikoh <anas.shikoh@kukufm.com>
2025-10-12 23:16:38 +0530

    import fix

--- app/src/main/java/com/yogeshpaliyal/deepr/ui/components/ActionIcon.kt
+++ app/src/main/java/com/yogeshpaliyal/deepr/ui/components/ActionIcon.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
