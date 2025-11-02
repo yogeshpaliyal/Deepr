920be283fd9e8867947e355244c6a42af79bf8b4
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2025-11-02 15:18:37 +0530

    Implement analytics manager factory and integration in application (#257)

--- app/src/proFreePlaystore/java/com/yogeshpaliyal/deepr/analytics/AnalyticsManagerFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.yogeshpaliyal.deepr.analytics
-
-import android.content.Context
-
-object AnalyticsManagerFactory {
-    fun create(context: Context): AnalyticsManager = FirebaseAnalyticsManager(context)
-}
