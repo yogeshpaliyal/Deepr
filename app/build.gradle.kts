68eea6f2a1414b2b81d469c0467752dd900265f7
Yogesh Choudhary Paliyal <yogeshpaliyal@users.noreply.github.com>
2026-01-16 17:08:35 +0000

    Github Actions: App version Bumped to v1.0.22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.yogeshpaliyal.deepr"
         minSdk = 24
         targetSdk = 36
-        versionCode = 22
-        versionName = "1.0.21"
+        versionCode = 23
+        versionName = "1.0.22"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments += mapOf(
