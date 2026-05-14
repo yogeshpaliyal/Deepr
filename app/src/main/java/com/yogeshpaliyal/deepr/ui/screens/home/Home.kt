2fe8ce32e29e11141405028c23e4bcb131ec6431
ALE-ARME <alessioarm69@gmail.com>
2026-05-14 21:26:04 +0200

    Fix: add missing imports for Spacer and width in dialogs

--- app/src/main/java/com/yogeshpaliyal/deepr/ui/screens/home/Home.kt
+++ app/src/main/java/com/yogeshpaliyal/deepr/ui/screens/home/Home.kt
@@ -22,7 +22,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
