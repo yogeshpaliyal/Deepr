86cc4179ff35f0bf65c768cc4fb930c6ad42fb5f
ALE-ARME <alessioarm69@gmail.com>
2026-05-16 16:18:57 +0200

    Fix #397: Improve title fetching for Reddit and Silent Save

--- app/src/main/java/com/yogeshpaliyal/deepr/data/NetworkRepository.kt
+++ app/src/main/java/com/yogeshpaliyal/deepr/data/NetworkRepository.kt
@@ -4,26 +4,34 @@ import com.yogeshpaliyal.deepr.util.normalizeLink
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
+import io.ktor.client.request.header
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpHeaders
 
 class NetworkRepository(
     val httpParser: HtmlParser,
+    private val httpClient: HttpClient = HttpClient(CIO),
 ) {
-    val httpClient: HttpClient by lazy {
-        HttpClient(CIO)
-    }
-
     suspend fun getLinkInfo(url: String): Result<LinkInfo> {
         val normalizedUrl = normalizeLink(url)
         try {
             val response =
                 httpClient.get(normalizedUrl) {
+                    header(
+                        HttpHeaders.UserAgent,
+                        "facebookexternalhit/1.1",
+                    )
                 }
             if (response.status.value != 200) {
                 return Result.failure(Exception("Failed to fetch data from $normalizedUrl, status code: ${response.status.value}"))
             }
             // Return the response body as text
-            return Result.success(httpParser.getTitleAndImageFromHtml(response.bodyAsText()))
+            return Result.success(
+                httpParser.getTitleAndImageFromHtml(
+                    response.bodyAsText(),
+                    normalizedUrl,
+                ),
+            )
         } catch (e: Exception) {
             return Result.failure(
                 Exception(
