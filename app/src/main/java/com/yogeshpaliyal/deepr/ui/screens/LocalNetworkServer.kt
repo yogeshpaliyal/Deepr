f7438a22807dc57bdc793b8e943d76404502df7b
ALE-ARME <alessioarm69@gmail.com>
2026-06-02 11:36:57 +0200

    New feature: Local Server UI - Display profiles API endpoints in LocalNetworkServer and translate to all languages

--- app/src/main/java/com/yogeshpaliyal/deepr/ui/screens/LocalNetworkServer.kt
+++ app/src/main/java/com/yogeshpaliyal/deepr/ui/screens/LocalNetworkServer.kt
@@ -335,6 +335,16 @@ fun LocalNetworkServerScreen(
                         )
                         ApiEndpointItem(
                             "GET",
+                            "/api/profiles",
+                            stringResource(R.string.api_get_profiles),
+                        )
+                        ApiEndpointItem(
+                            "POST",
+                            "/api/profiles",
+                            stringResource(R.string.api_add_profile),
+                        )
+                        ApiEndpointItem(
+                            "GET",
                             "/api/tags",
                             stringResource(R.string.api_get_tags),
                         )
